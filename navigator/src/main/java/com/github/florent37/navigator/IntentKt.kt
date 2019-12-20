25f23726ab1122a01287ba8acd81180a9bafdf25
Florent Champigny <florent.champigny@idean.com>
2019-12-20 15:27:58 +0100

    updated README

--- navigator/src/main/java/com/github/florent37/navigator/IntentKt.kt
+++ navigator/src/main/java/com/github/florent37/navigator/IntentKt.kt
@@ -3,6 +3,12 @@ package com.github.florent37.navigator
 import android.app.Activity
 import android.content.Intent
 
+/**
+ * Updated an intent adding all extras of given arg
+ * from Activity :
+ *
+ * this.updateIntent(newIntent)
+ */
 fun Activity.updateIntent(newIntent: Intent?){
     this.intent = this.intent?.updateWith(newIntent)
 }
